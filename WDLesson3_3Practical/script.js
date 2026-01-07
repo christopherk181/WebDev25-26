46b8e781b75bfef04eb2006f046c24bd436960d9
christopherk181 <christopherk181@nycstudents.net>
2026-01-07 19:10:15 +0000

    sdfgh

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,4 +1,16 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
 function Knight1(){
-    document.getElementById
+    document.getElementById("highlight").innerHTML = "This is a knight whos armor shines but hides in the dark.";
+}
+function Knight2(){
+    document.getElementById("highlight").innerHTML = "This is a knight that killed an ork.";
+}
+function Knight3(){
+    document.getElementById("highlight").innerHTML = "This is a knight who survived a war.";
+}
+function Knight4(){
+    document.getElementById("highlight").innerHTML = "This is a knight who is sad about his fallen comrades.";
+}
+function Knight5(){
+    document.getElementById("highlight").innerHTML = "This is a knight is ready for battle.";
 }
