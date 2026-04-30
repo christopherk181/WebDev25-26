a8256741c433d90052367f8c72ca237a62d6aca5
christopherk181 <christopherk181@nycstudents.net>
2026-04-30 18:09:42 +0000

    9766

--- WDLesson6_7Practical /script.js
+++ WDLesson6_7Practical /script.js
@@ -14,12 +14,12 @@ async function init(){
                  <h3>${complaint.complaint_type}</h3>
                  <hr>
                  <p>${complaint.borough}</p>
-                 <p>${complaint.incident_zip}</p>
-                 <p>${complaint.descriptor}</p>
+                 <p>${complaint.zip_code}</p>
+                 <p>${complaint.crash_time}</p>
                  <hr>
-                 <p>${complaint.created_date}</p>
+                 <p>${complaint.crash_date}</p>
                  <hr>
-                 <p>${complaint.agency}</p>
+                 <p>${complaint.latitude}</p>
               </div>`    
   }
   output.innerHTML = build;
@@ -42,11 +42,11 @@ function filterByBorough(){
                     <hr>
                     <p>${complaint.borough}</p>
                     <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
+                    <p>${complaint.crash_time}</p>
                     <hr>
-                    <p>${complaint.created_date}</p>
+                    <p>${complaint.crash_date}</p>
                     <hr>
-                    <p>${complaint.agency}</p>
+                    <p>${complaint.latitude}</p>
                 </div>`;
       ct += 1;
     }
@@ -56,27 +56,25 @@ function filterByBorough(){
 }
 
 // Challenge 2: Create an event handler (function) to filter the 311 Service Request by zip code.
-function searchZip(){
+function searchzip(){
   let zip= document.getElementById("zip").value;
-  let output = document.getElementById("output").value;
   let result = document.getElementById("result");
-
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
-    let complaint = data[i];
-    if(complaint.zip == zip){
+    let crash = data[i];
+    if(crash.zip == zip){
       build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
+                    <h3>${crash.crash_type}</h3>
                     <hr>
-                    <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
+                    <p>${crash.borough}</p>
+                    <p>${crash.zip_code}</p>
+                    <p>${crash.descriptor}</p>
                     <hr>
-                    <p>${complaint.created_date}</p>
+                    <p>${crash.created_date}</p>
                     <hr>
-                    <p>${complaint.agency}</p>
+                    <p>${crash.agency}</p>
                 </div>`;
       ct += 1;
     }
@@ -89,13 +87,14 @@ function searchZip(){
 
 // Challenge 4: Create an event handler (function) to filter the 311 Service Request by complaint type.
 
-function searchcomplaint(){
-  let issue= document.getElementById("complaint").value;
-  let output = document.getElementById("output").value;
+function searchcollision(){
+  let collision = document.getElementById("collision").value;
   let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
 
   async function init(){   
-  let link = "311.json"; //let link = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=1000";
+  let link = "mvc.json"; //let link = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=1000";
   info = await fetch(link);
   data = await info.json();
   
@@ -103,17 +102,17 @@ function searchcomplaint(){
   let build = "";
 
   for(let i = 0; i < data.length; i+=1){
-    let complaint = data[i];
+    let collision = data[i];
     build += `<div class="fitted card">
-                 <h3>${complaint.complaint_type}</h3>
+                 <h3>${collision.complaint_type}</h3>
                  <hr>
-                 <p>${complaint.borough}</p>
-                 <p>${complaint.incident_zip}</p>
-                 <p>${complaint.descriptor}</p>
+                 <p>${collision.borough}</p>
+                 <p>${collision.incident_zip}</p>
+                 <p>${collision.descriptor}</p>
                  <hr>
-                 <p>${complaint.created_date}</p>
+                 <p>${collision.created_date}</p>
                  <hr>
-                 <p>${complaint.agency}</p>
+                 <p>${collision.agency}</p>
               </div>`    
   }
   output.innerHTML = build;
